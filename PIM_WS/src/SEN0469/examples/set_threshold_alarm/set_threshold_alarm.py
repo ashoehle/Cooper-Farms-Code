67d4d4f977462b56a211995605905671c29c7c82
mjdaanen <mjdaanen@mtu.edu>
2026-09-18 18:57:07 -0400

    forgot to save code from last commit, refer to last commit for details

--- PIM_WS/src/SEN0469/examples/set_threshold_alarm/set_threshold_alarm.py
+++ PIM_WS/src/SEN0469/examples/set_threshold_alarm/set_threshold_alarm.py
@@ -48,7 +48,7 @@ ctype=0
 
 if ctype==0:
   I2C_1       = 0x01               # I2C_1 Use i2c1 interface (or i2c0 with configuring Raspberry Pi) to drive sensor
-  I2C_ADDRESS = 0x77               # I2C Device address, which can be changed by changing A1 and A0, the default address is 0x77
+  I2C_ADDRESS = 0x74               # I2C Device address, which can be changed by changing A1 and A0, the default address is 0x77
   gas = DFRobot_MultiGasSensor_I2C(I2C_1 ,I2C_ADDRESS)
 else:
   gas = DFRobot_MultiGasSensor_UART(9600)
